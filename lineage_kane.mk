7928daf147683df27a8e5010cb7572c08798128b
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-10 13:59:50 -0400

    kane: Cleanup
    Change-Id: I9762c939670b4cce5903f9dc77bca0e58dbfce81

--- lineage_kane.mk
+++ lineage_kane.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,18 +14,18 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
+## Inherit from generic products, most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Inherit from kane device
+## Inherit from kane device
 $(call inherit-product, device/motorola/kane/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
+## Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Device identifier. This must come after all inclusions
+## Device identifier, this must come after all inclusions
 PRODUCT_DEVICE := kane
 PRODUCT_NAME := lineage_kane
 PRODUCT_BRAND := Motorola
@@ -34,7 +34,7 @@ PRODUCT_MANUFACTURER := Motorola
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-# Use the latest approved GMS identifiers
+## Use the latest CTS approved build identifiers
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=kane_sprout \
     PRIVATE_BUILD_DESC="10/QSAS30.62-33-4/2d332:user/release-keys"
