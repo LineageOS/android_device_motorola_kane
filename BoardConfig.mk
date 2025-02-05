d81aa5bc42a046ce26653f135df419b8bafa204a
Jan Altensen <info@stricted.net>
2025-02-06 00:41:47 +0100

    kane: build camera hal from source
    Change-Id: I0ce22d2d734866c59944c0f20ad88e5fa7fab45c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,12 @@ DEVICE_PATH := device/motorola/kane
 ## Assert
 TARGET_OTA_ASSERT_DEVICE := kane,one_vision
 
+# Camera
+$(call soong_config_set,exynos_camera,front_camera_sensor,SENSOR_NAME_S5K2X5SP)
+$(call soong_config_set,exynos_camera,back_camera_sensor,SENSOR_NAME_S5KGM1SP)
+$(call soong_config_set,exynos_camera,back_1_camera_sensor,SENSOR_NAME_S5K5E9)
+$(call soong_config_set,exynos_camera,target_variant,kane)
+
 ## Kernel
 TARGET_KERNEL_CONFIG := kane_defconfig
 
