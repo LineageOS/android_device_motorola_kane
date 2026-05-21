c1bf1a6c44530821b98b4a06447cedbf7ff7c263
Nolen Johnson <johnsonnolen@gmail.com>
2026-05-21 01:46:59 -0400

    kane: Move soong_config_set{,_bool}'s to product.mk
    * Allowing these in BoardConfig*.mk is a LineageOS addition,
      it is unecessary to store these here.
    * Re-sort other ones while here and seperate out bool entries.
    
    Change-Id: I29c8348cda82ba0890b6db3819c9ea4d7f2fac89

--- device.mk
+++ device.mk
@@ -9,6 +9,12 @@ $(call inherit-product, device/motorola/exynos9610-common/common.mk)
 # Inherit proprietary files
 $(call inherit-product, vendor/motorola/kane/kane-vendor.mk)
 
+# Camera
+$(call soong_config_set,exynos_camera,back_1_camera_sensor,SENSOR_NAME_S5K5E9)
+$(call soong_config_set,exynos_camera,back_camera_sensor,SENSOR_NAME_S5KGM1SP)
+$(call soong_config_set,exynos_camera,front_camera_sensor,SENSOR_NAME_S5K2X5SP)
+$(call soong_config_set,exynos_camera,target_variant,kane)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
